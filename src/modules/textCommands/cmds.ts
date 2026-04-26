d7e0a4fcd78a02e9b10e931f1a563f2e6b5ec1b3
Tally <113806118+tallypaws@users.noreply.github.com>
2026-04-26 04:14:25 -0400

    this mf forgot how to regex

--- src/modules/textCommands/cmds.ts
+++ src/modules/textCommands/cmds.ts
@@ -74,7 +74,7 @@ export const commands: CommandDef[] = [
           "Invalid prefix. Prefix length must be at most 32. (why the heck are you doing this)",
         );
       }
-      if (!/^[a-zA-Z0-9\.-_;:[]{}-=_+]+$/.test(prefix)) {
+      if (!/^[a-zA-Z0-9\.-_;:\[\]{}\-=_\+]+$/.test(prefix)) {
         throw new InvalidCommandUsageError(
           "Invalid prefix. Prefix must only contain alphanumeric characters and `.-_;:[]{}-=_+`.",
         );
